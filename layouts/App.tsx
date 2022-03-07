0aeae04041770e07cafb531acda3b231a197c323
amagramer <amagramer@gmail.com>
2022-03-07 19:28:33 +0900

    react router v6 적용

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>App 컴포넌트</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
